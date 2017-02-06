e05c34674b7c2475c28fa67cd1c3e317118deb70
Ilija Vucetic <vuceticilija@gmail.com>
2017-02-06 23:41:19 +0100

    Lesson 95 finished. Finish implementation for creating new post and fetcching list of all posts. Displaying single post UI.

---
+++ src/components/posts_show.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index';
+
+class PostsShow extends Component {
+
+
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    render() {
+
+        if (!this.props.post) {
+            return <div>Loading...</div>
+        }
+
+        return <div>
+            <h3>{this.props.post.title}</h3>
+            <h6>Categories: {this.props.post.categories}</h6>
+            <p>{this.props.post.content}</p>
+
+        </div>
+    }
+}
+
+function mapStateToProps(state) {
+    return {post: state.posts.post};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
